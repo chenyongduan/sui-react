a5448ae507ce53110da5e7ea7137af357bfb148f
chenyongduan <250881711@qq.com>
2023-03-29 19:16:54 +0800

    feat: 2d射箭功能

--- src/pages/three-page/index.tsx
+++ src/pages/three-page/index.tsx
@@ -1,64 +1,96 @@
+import { Ref, RefObject, useEffect, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
-import { useRef, useState } from "react";
-import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
-import { Ring } from "@react-three/drei";
-import React from "react";
-import { Mesh } from "three";
+import { useSpring, a } from "@react-spring/three";
+import { OrbitControls } from "@react-three/drei";
 
-function Box(props: ThreeElements["mesh"]) {
-  const mesh = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
+type MeshType = THREE.Mesh<
+  THREE.BufferGeometry,
+  THREE.Material | THREE.Material[]
+>;
 
-  useFrame((state, delta) => {
-    mesh.current.rotation.x += delta;
-  });
-
+function Cube1({ onRef }: { onRef: Ref<MeshType | undefined> }) {
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    <mesh ref={onRef} position={[-6, 0, 0]}>
+      <boxGeometry args={[1.5, 0.05, 0.05]} />
+      <meshBasicMaterial color={0xff0000} />
     </mesh>
   );
 }
 
-function useTurntable() {
-  const ref = React.useRef<Mesh>(null!);
+function Cube2({ onRef }: { onRef: Ref<MeshType | undefined> }) {
+  return (
+    <a.mesh ref={onRef} position={[0, 0, 0]}>
+      <boxGeometry args={[1, 2, 1]} />
+      <meshBasicMaterial color={0x00ff00} />
+    </a.mesh>
+  );
+}
+
+function World() {
+  const worldRef = useRef<MeshType>();
+  const cube1Ref = useRef<MeshType>();
+  const cube2Ref = useRef<MeshType>();
+  const checkCollisionRef = useRef(true);
+  const cube1Box = new THREE.Box3();
+  const cube2Box = new THREE.Box3();
+  const setCube2Ref = useRef(true);
+  const cubeListRef = useRef<any>([]);
 
   useFrame(() => {
-    ref.current.rotation.x += 0.02;
+    if (cube1Ref.current) {
+      cube1Box.setFromObject(cube1Ref.current);
+    }
+    if (cube2Ref.current && setCube2Ref.current) {
+      setCube2Ref.current = false;
+      cube2Box.setFromObject(cube2Ref.current);
+    }
+    if (checkCollisionRef.current && cube2Box.intersectsBox(cube1Box)) {
+      checkCollisionRef.current = false;
+      if (cube1Ref.current) {
+        console.log("Collision detected!", cube1Ref.current);
+        const newCube = cube1Ref.current.clone(true);
+        const center = new THREE.Vector3();
+        cube2Box.getCenter(center);
+        console.log("Collision detected! newCube=", center);
+        cube1Ref.current.position.set(-1.25, cube1Ref.current.position.y, 0);
+        cube2Ref.current?.add(cube1Ref.current);
+        cubeListRef.current.push(cube1Ref.current);
+
+        if (cubeListRef.current.length > 5) {
+          const cube = cubeListRef.current.shift();
+          cube.removeFromParent();
+        }
+
+        newCube.name = "newCube";
+        cube1Ref.current = newCube;
+        cube1Ref.current.position.x = -6;
+        cube1Ref.current.position.y = Math.random();
+        worldRef.current?.add(cube1Ref.current);
+        checkCollisionRef.current = true;
+      }
+    } else {
+      if (cube1Ref.current && checkCollisionRef.current) {
+        cube1Ref.current.position.x += 0.04;
+      }
+    }
   });
 
-  return ref;
-}
-
-function Story({ args }: { args?: any }) {
-  const ref = useTurntable();
-
   return (
-    <Ring ref={ref} args={args}>
-      <meshPhysicalMaterial color="#ff0000" wireframe wireframeLinewidth={10} />
-    </Ring>
+    <mesh ref={worldRef}>
+      <Cube1 onRef={cube1Ref} />
+      <Cube2 onRef={cube2Ref} />
+    </mesh>
   );
 }
 
-const ThreePage = () => {
+function App() {
   return (
     <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 2, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <Story />
+      <OrbitControls />
+      <World />
     </Canvas>
   );
-};
+}
 
-export default ThreePage;
+export default App;
