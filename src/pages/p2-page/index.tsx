06292c142a4ea060f7314b9704c4c29d7cdf36d6
chenyongduan <250881711@qq.com>
2023-03-30 19:08:19 +0800

    feat: 添加射击效果

--- src/pages/p2-page/index.tsx
+++ src/pages/p2-page/index.tsx
@@ -1,56 +1,107 @@
 import { useTexture, Html } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Physics, useBox, usePlane, useLine, Debug } from "@react-three/p2";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Physics, useBox, usePlane, Debug } from "@react-three/p2";
 import { Button } from "antd";
-import { Ref, useCallback, useEffect, useState } from "react";
+import { Ref, useCallback, useEffect, useRef, useState } from "react";
 import { BufferGeometry, Material, Mesh } from "three";
 
 type MeshRef = Ref<Mesh<BufferGeometry, Material | Material[]>>;
 
-function Arrow({ reset }: { reset: boolean }) {
-  const [ref, api] = useLine(() => ({
-    type: "Dynamic",
-    args: [1],
-    mass: 1,
-  }));
-
-  useEffect(() => {
-    api.position.set(0, 5);
-    api.velocity.set(0, 0);
-  }, [reset]);
-
+function Circle() {
   return (
-    <mesh
-      ref={ref as MeshRef}
-      name="arrow"
-      onPointerEnter={() => {
-        // api.angle.set(20);
-      }}
-      onPointerDown={() => {
-        api.velocity.set(20, 20);
-      }}
-    >
-      <boxGeometry args={[2, 0.2, 0.1]} />
-      <meshStandardMaterial color={"blue"} />
+    <mesh>
+      <circleGeometry />
+      <meshBasicMaterial color="red" />
     </mesh>
   );
 }
 
-function Enemy({ reset }: { reset: boolean }) {
+function Arrow({ reset }: { reset: boolean }) {
+  const cylinderRef = useRef<Mesh<BufferGeometry, Material | Material[]>>();
+  const [cylinderVisible, setCylinderVisible] = useState(false);
+
   const [ref, api] = useBox(() => ({
     type: "Dynamic",
+    args: [2, 0.2],
+    mass: 1,
+    onCollide: (e) => {
+      if (e.body.name === "enemy") {
+        if (cylinderRef.current) {
+          setCylinderVisible(true);
+          ref.current?.add(cylinderRef.current);
+          api.sleep();
+        }
+      }
+    },
+  }));
+
+  useEffect(() => {
+    api.position.set(-10, -5);
+    api.velocity.set(0, 0);
+  }, [reset]);
+
+  return (
+    <>
+      <mesh
+        ref={ref as MeshRef}
+        name="arrow"
+        onPointerEnter={() => {
+          // api.angle.set(20);
+        }}
+        onPointerDown={() => {
+          api.velocity.set(20, 20);
+        }}
+      >
+        <boxGeometry args={[2, 0.2, 0.1]} />
+        <meshStandardMaterial color={"blue"} />
+      </mesh>
+
+      <mesh ref={cylinderRef} position={[0, 0.5, 0]} visible={cylinderVisible}>
+        <cylinderGeometry args={[0.5, 0.5, 1, 32]} />
+        <meshBasicMaterial color={0x00ff00} />
+      </mesh>
+    </>
+  );
+}
+
+function Enemy({ reset }: { reset: boolean }) {
+  const angleRef = useRef(0);
+  const posRef = useRef([0, -4]);
+
+  const [ref, api] = useBox(() => ({
+    type: "Kinematic",
     mass: 1,
     args: [5, 5],
-    // position: [0, 0],
     onCollide: (e) => {
       console.log("Enemy=", e);
     },
   }));
 
   useEffect(() => {
-    api.position.set(0, 0);
+    api.position.set(0, -4);
     // api.velocity.set(0, 5);
   }, [reset]);
+
+  useEffect(() => {
+    const subscribe = api.angle.subscribe((value) => {
+      angleRef.current = value;
+    });
+    return subscribe;
+  }, []);
+
+  useEffect(() => {
+    const subscribe = api.position.subscribe((value) => {
+      posRef.current = value;
+    });
+    return subscribe;
+  }, []);
+
+  useFrame(() => {
+    api.angle.set((angleRef.current += 0.005));
+
+    // api.position.set(posRef.current[0] + 0.01, posRef.current[1]);
+    // api.velocity.set(1, 1);
+  });
 
   return (
     <mesh ref={ref as MeshRef} name="enemy">
@@ -65,6 +116,7 @@ function Floor() {
     type: "Static",
     position: [0, -7],
     onCollideBegin: (e) => {
+      // console.log("=======floor");
       // e.body.position.set(0, 0, 0);
     },
   }));
@@ -110,7 +162,7 @@ export default function App() {
             <meshPhongMaterial color="#374037" />
           </mesh>
           {/* @ts-ignore */}
-          <Debug scale={1.1} linewidth={0} normalIndex={2}>
+          <Debug scale={1.1} linewidth={0.005} normalIndex={2}>
             <Floor />
             <Arrow reset={reset} />
             <Enemy reset={reset} />
--- src/pages/three-page/index.tsx
+++ src/pages/three-page/index.tsx
@@ -1,93 +1,231 @@
-import { Ref, RefObject, useEffect, useRef } from "react";
+import { Ref, useEffect, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
-import { useSpring, a } from "@react-spring/three";
-import { OrbitControls } from "@react-three/drei";
+import { SpringValue, a, useSpring } from "@react-spring/three";
+import { PerspectiveCamera, useTexture } from "@react-three/drei";
+import { useGesture } from "@use-gesture/react";
 
 type MeshType = THREE.Mesh<
   THREE.BufferGeometry,
   THREE.Material | THREE.Material[]
 >;
 
-function Cube1({ onRef }: { onRef: Ref<MeshType | undefined> }) {
+function Arrow({ onRef }: { onRef: Ref<MeshType | undefined> }) {
+  const spearMap = useTexture("/spear.png");
   return (
-    <mesh ref={onRef} position={[-6, 0, 0]}>
-      <boxGeometry args={[1.5, 0.05, 0.05]} />
-      <meshBasicMaterial color={0xff0000} />
-    </mesh>
+    <a.mesh ref={onRef}>
+      <boxGeometry args={[0.3, 2.5, 0.01]} />
+      <meshBasicMaterial map={spearMap} transparent depthWrite={false} />
+    </a.mesh>
   );
 }
 
-function Cube2({ onRef }: { onRef: Ref<MeshType | undefined> }) {
+function Enemy({ onRef }: { onRef: Ref<MeshType | undefined> }) {
   return (
-    <a.mesh ref={onRef} position={[0, 0, 0]}>
-      <boxGeometry args={[1, 2, 1]} />
+    <a.mesh ref={onRef} position={[4, 1, 0]}>
+      <boxGeometry args={[4, 4, 0.01]} />
       <meshBasicMaterial color={0x00ff00} />
     </a.mesh>
   );
 }
 
+function Player({
+  onRef,
+  rotationZ,
+}: {
+  onRef: Ref<MeshType | undefined>;
+  rotationZ: SpringValue<number>;
+}) {
+  return (
+    <a.mesh ref={onRef} position={[-5, 0, -0.1]} rotation-z={rotationZ}>
+      <boxGeometry args={[1, 2, 0.01]} />
+      <meshBasicMaterial color={"#ff6d6d"} />
+    </a.mesh>
+  );
+}
+
+function LineDashed() {
+  const ref = useRef();
+
+  useEffect(() => {
+    // ref.current;
+  }, []);
+
+  return (
+    <mesh ref={ref}>
+      {/* <bufferGeometry />
+      <lineDashedMaterial
+        color="blue"
+        linewidth={1}
+        scale={1}
+        dashSize={1}
+        gapSize={0.5}
+      /> */}
+    </mesh>
+  );
+}
+
+function mapRange(
+  value: number,
+  min1: number,
+  max1: number,
+  min2: number,
+  max2: number
+) {
+  return Math.min(
+    max2,
+    min2 + ((max2 - min2) * (value - min1)) / (max1 - min1)
+  );
+}
+
 function World() {
   const worldRef = useRef<MeshType>();
-  const cube1Ref = useRef<MeshType>();
-  const cube2Ref = useRef<MeshType>();
+  const arrowRef = useRef<MeshType>();
+  const enemyRef = useRef<MeshType>();
+  const playerRef = useRef<MeshType>();
   const checkCollisionRef = useRef(true);
-  const cube1Box = new THREE.Box3();
-  const cube2Box = new THREE.Box3();
-  const setCube2Ref = useRef(true);
-  const cubeListRef = useRef<any>([]);
+  const arrowBox = new THREE.Box3();
+  const enemyBox = new THREE.Box3();
+  const setEnemyRef = useRef(true);
+  const arrowListRef = useRef<any>([]);
+  const ARROW_ROTATION_Z = -Math.PI / 1.8;
+
+  const [{ rotationZ, arrowRotationZ }, set] = useSpring(() => ({
+    rotationZ: 0,
+    arrowRotationZ: ARROW_ROTATION_Z,
+  }));
+
+  const [{ arrowX, arrowY }, arrowApi] = useSpring(() => ({
+    arrowX: -5,
+    arrowY: 1,
+    config: {
+      duration: 3000,
+    },
+  }));
+
+  const bind = useGesture({
+    onDrag: ({ down, movement }) => {
+      let rotationZ = 0;
+      let arrowRotationZ = ARROW_ROTATION_Z;
+      if (down) {
+        const offsetX = movement[0];
+        let playerRotation = 0;
+        let arrowRotation = 0;
+        if (offsetX > 0) {
+          arrowRotation = -mapRange(offsetX, 0, 20, 0, 1.2);
+          playerRotation = mapRange(offsetX, 0, 20, 0, 6);
+        } else if (offsetX < 0) {
+          arrowRotation = mapRange(-offsetX, 0, 20, 0, 3);
+        }
+        rotationZ = Math.PI / (7 + playerRotation);
+        arrowRotationZ = -Math.PI / (3 + arrowRotation);
+      } else {
+        checkCollisionRef.current = true;
+        arrowApi({
+          arrowX: 10,
+        });
+      }
+      set({
+        rotationZ,
+        arrowRotationZ,
+      });
+    },
+  });
 
   useFrame(() => {
-    if (cube1Ref.current) {
-      cube1Box.setFromObject(cube1Ref.current);
+    if (arrowRef.current) {
+      arrowBox.setFromObject(arrowRef.current);
     }
-    if (cube2Ref.current && setCube2Ref.current) {
-      setCube2Ref.current = false;
-      cube2Box.setFromObject(cube2Ref.current);
+    if (enemyRef.current && setEnemyRef.current) {
+      setEnemyRef.current = false;
+      enemyBox.setFromObject(enemyRef.current);
     }
-    if (checkCollisionRef.current && cube2Box.intersectsBox(cube1Box)) {
+    if (checkCollisionRef.current && enemyBox.intersectsBox(arrowBox)) {
+      console.log("=======collision");
       checkCollisionRef.current = false;
-      if (cube1Ref.current) {
-        console.log("Collision detected!", cube1Ref.current);
-        const newCube = cube1Ref.current.clone(true);
-        const center = new THREE.Vector3();
-        cube2Box.getCenter(center);
-        console.log("Collision detected! newCube=", center);
-        cube1Ref.current.position.set(-1.25, cube1Ref.current.position.y, 0);
-        cube2Ref.current?.add(cube1Ref.current);
-        cubeListRef.current.push(cube1Ref.current);
+      if (arrowRef.current) {
+        const newArrow = arrowRef.current.clone(true);
+        arrowRef.current.position.set(-3, 0, 0);
+        enemyRef.current?.add(arrowRef.current);
+        arrowListRef.current.push(arrowRef.current);
 
-        if (cubeListRef.current.length > 5) {
-          const cube = cubeListRef.current.shift();
+        arrowApi.stop();
+        if (arrowListRef.current.length > 5) {
+          const cube = arrowListRef.current.shift();
           cube.removeFromParent();
         }
 
-        newCube.name = "newCube";
-        cube1Ref.current = newCube;
-        cube1Ref.current.position.x = -6;
-        cube1Ref.current.position.y = Math.random();
-        worldRef.current?.add(cube1Ref.current);
-        checkCollisionRef.current = true;
+        newArrow.name = "newArrow";
+        arrowRef.current = newArrow;
+        arrowApi({ arrowX: -5, arrowY: 1, config: { duration: 10 } });
+        worldRef.current?.add(arrowRef.current);
       }
     } else {
-      if (cube1Ref.current && checkCollisionRef.current) {
-        cube1Ref.current.position.x += 0.04;
-      }
+      arrowRef.current?.rotation.set(0, 0, arrowRotationZ.get());
+      arrowRef.current?.position.set(arrowX.get(), arrowY.get(), 0);
     }
   });
 
+  useEffect(() => {
+    var geometry = new THREE.BufferGeometry();
+    var curve = new THREE.CatmullRomCurve3([
+      new THREE.Vector3(-2, 0, 0),
+      new THREE.Vector3(0, 3, 0),
+      new THREE.Vector3(2, 0, 0),
+    ]);
+    var points = curve.getPoints(100);
+    geometry.setFromPoints(points);
+    //材质对象
+    var material = new THREE.LineDashedMaterial({
+      color: 0xff0000,
+      linewidth: 2,
+      scale: 1,
+      dashSize: 0.1,
+      gapSize: 0.2,
+    });
+    //线条模型对象
+    var line = new THREE.Line(geometry, material);
+    line.computeLineDistances();
+    worldRef.current?.add(line);
+  }, []);
+
   return (
-    <mesh ref={worldRef}>
-      <Cube1 onRef={cube1Ref} />
-      <Cube2 onRef={cube2Ref} />
-    </mesh>
+    <a.mesh ref={worldRef} {...bind()}>
+      <Arrow onRef={arrowRef} />
+      <Enemy onRef={enemyRef} />
+      <Player onRef={playerRef} rotationZ={rotationZ} />
+      {/* <LineDashed /> */}
+    </a.mesh>
   );
 }
 
 function App() {
   return (
     <Canvas>
-      <OrbitControls />
+      <PerspectiveCamera makeDefault position={[0, 2, 10]} />
+      <ambientLight intensity={0.5} />
+      <spotLight
+        position={[0, 15, 10]}
+        angle={0.6}
+        penumbra={1}
+        intensity={1}
+        shadow-mapSize={[2048, 2048]}
+        shadow-bias={-0.0001}
+      />
+      {/* 背景 */}
+      <mesh position={[0, 0, -2]}>
+        <planeGeometry args={[1000, 1000]} />
+        <meshPhongMaterial color="#87CEEB" />
+      </mesh>
+      {/* 地板 */}
+      <mesh
+        position={[0, -1, -1]}
+        receiveShadow
+        rotation={[-Math.PI / 3, 0, 0]}
+      >
+        <planeGeometry args={[1000, 1000]} />
+        <meshPhongMaterial color="#F5DEB3" />
+      </mesh>
       <World />
     </Canvas>
   );
